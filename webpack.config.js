60c365c67a03e69a58aa3325add519ea3d8697e6
A-Hsien <HsiaoHsien0325@gmail.com>
2020-02-04 00:09:11 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const { CheckerPlugin } = require('awesome-typescript-loader');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { optimize } = require('webpack');
+const { join } = require('path');
+const prodPlugins = [];
+if (process.env.NODE_ENV === 'production') {
+  prodPlugins.push(
+    new optimize.AggressiveMergingPlugin(),
+    new optimize.OccurrenceOrderPlugin()
+  );
+}
+module.exports = {
+  mode: process.env.NODE_ENV,
+  devtool: 'inline-source-map',
+  entry: {
+    contentscript: join(__dirname, 'src/contentscript/main.ts'),
+    background: join(__dirname, 'src/background/main.ts'),
+    popup: join(__dirname, 'src/popup/main.ts')
+  },
+  output: {
+    path: join(__dirname, 'dist'),
+    filename: '[name].js',
+  },
+  module: {
+    rules: [
+      {
+        exclude: /node_modules/,
+        test: /\.ts?$/,
+        use: 'awesome-typescript-loader?{configFileName: "tsconfig.json"}',
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new CheckerPlugin(),
+    ...prodPlugins,
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+  ],
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+};
