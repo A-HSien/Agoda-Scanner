0a43b993e43c4f94241c235eb0a0cb110b118386
a-hsien <hsiaohsien0325@gmail.com>
2020-02-07 23:51:53 +0800

    adjust git ignore.

---
+++ build/precache-manifest.cbf5a7490cc528ef187a5e3aedca1262.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5a1d588500d003bfe45d2b3ca987fb1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d5b54e52565c94fefe8",
+    "url": "/static/css/main.9139e296.chunk.css"
+  },
+  {
+    "revision": "34aa7c0ea465a86f13f4",
+    "url": "/static/js/2.635aefbd.chunk.js"
+  },
+  {
+    "revision": "de1cedeacf4641d6277b42334cfacb1d",
+    "url": "/static/js/2.635aefbd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d5b54e52565c94fefe8",
+    "url": "/static/js/main.08e39b55.chunk.js"
+  },
+  {
+    "revision": "8866df7ea17ccf5e942d",
+    "url": "/static/js/runtime-main.cc700f05.js"
+  }
+]);
